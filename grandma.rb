85e5e9963870899cb02517ea22801c49fbdfbc14
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-15 17:42:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
  if phrase = "I love you grandma!".upcase
-   puts "i love you to pumpkin!".upcase
+   puts "i love you too pumpkin!".upcase
   elsif 
   puts "huh?! speak up, sonny!".upcase
   else
