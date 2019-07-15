d432a6d8d8f29a041d39b80d351131911b17df9e
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-15 17:45:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
- if phrase = "I LOVE YOU TOO PUMPKIN!"
+ if phrase == "I LOVE YOU TOO PUMPKIN!"
    "i love you too pumpkin!".upcase
-  elsif phrase = "Hi Nana, how are you?"
+  elsif phrase == "Hi Nana, how are you?"
   "huh?! speak up, sonny!".upcase
   else
     "No, not since 1938!".upcase
